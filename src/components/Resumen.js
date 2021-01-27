02de2464e6000ec9a077e15ad0cbed0bb75115ff
Christopher Solis <christopher.solis@edify.cr>
2021-01-26 19:41:32 -0600

    Initial commit

---
+++ src/components/Resumen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { primerMayuscula } from "../helper";
+import styled from "@emotion/styled";
+
+const ContenedorResumen = styled.div`
+  padding: 1rem;
+  text-align: center;
+  background-color: #00838f;
+  color: #ffffff;
+  margin-top: 1rem;
+`;
+
+const Resumen = ({ datos }) => {
+  const { marca, year, plan } = datos;
+  if (marca === "" || year === "" || plan === "") return null;
+
+  return (
+    <ContenedorResumen>
+      <h2>Resumen de Cotización</h2>
+      <ul>
+        <li>Marca: {primerMayuscula(marca)}</li>
+        <li>Plan: {primerMayuscula(plan)}</li>
+        <li>Año del Auto: {year}</li>
+      </ul>
+    </ContenedorResumen>
+  );
+};
+
+Resumen.propTypes = {
+  datos: PropTypes.object.isRequired,
+};
+
+export default Resumen;
